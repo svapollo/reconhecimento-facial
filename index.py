654a3da754d3bf60e2abeb2d97b53213b588dd5d
Apollo <sv.apollo11@gmail.com>
2023-03-05 14:13:10 -0300

    cria a base das faces para comparar

--- index.py
+++ index.py
@@ -1,15 +1,32 @@
 import boto3
 
 s3 = boto3.resource('s3')
+client = boto3.client('rekognition')
 
 
 def lista_imagens():
     imagens = []
-    bucket = s3.Bucket('inicio-reconhecimento')
+    bucket = s3.Bucket('reconhecimento-inicio')
     for imagem in bucket.objects.all():
         imagens.append(imagem.key)
-    print(imagens)
     return imagens
 
 
+def indexa_colecao(imagens):
+    for i in imagens:
+        response = client.index_faces(
+            CollectionId='faces',
+            DetectionAttributes=[
+            ],
+            ExternalImageId=i[:-4],
+            Image={
+                'S3Object': {
+                    'Bucket': 'reconhecimento-inicio',
+                    'Name': i,
+                },
+            },
+        )
+
+
 imagens = lista_imagens()
+indexa_colecao(imagens)
