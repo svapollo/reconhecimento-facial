e8e71208886f02dc24f32a0bc2aa279c2f318f84
Apollo <sv.apollo11@gmail.com>
2023-03-06 21:55:07 -0300

    feat() publica resultado do reconhecimento facial

--- faceanalise.py
+++ faceanalise.py
@@ -54,8 +54,14 @@ def gera_dados_json(resultado_comparacao):
     return dados_json
 
 
+def publica_dados(dados_json):
+    arquivo = s3.Object('site-reconhecimento', 'dados.json')
+    arquivo.put(Body=json.dumps(dados_json))
+
+
 faces_detectadas = detecta_faces()
 faceId_detectadas = cria_lista_faceId_detectadas(faces_detectadas)
 resultado_comparacao = compara_imagens(faceId_detectadas)
 dados_json = gera_dados_json(resultado_comparacao)
+publica_dados(dados_json)
 print(json.dumps(dados_json, indent=4))
