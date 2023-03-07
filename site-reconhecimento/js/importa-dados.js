251958cdd797653fc4c08221f9ad177c88bf09fd
Apollo <sv.apollo11@gmail.com>
2023-03-06 21:55:35 -0300

    fix() caminho bucket site e imagens

--- site-reconhecimento/js/importa-dados.js
+++ site-reconhecimento/js/importa-dados.js
@@ -1,6 +1,6 @@
 
 $.ajax(
-  { url: 'https://s3.amazonaws.com/site/dados.json',
+  { url: 'https://s3.amazonaws.com/site-reconhecimento/dados.json',
    dataType: 'json',
    crossDomain: true,
    success: function (dados) {
@@ -26,7 +26,7 @@ $.ajax(
       tdInfoFoto = document.createElement("img");
       tdInfoFoto.height = 100;
       tdInfoFoto.width = 68;
-      tdInfoFoto.src = 'https://s3.amazonaws.com/reconhecimento-inicio/' + dados.nome + '.jpg';
+      tdInfoFoto.src = 'https://s3.amazonaws.com/reconhecimento-inicio/' + dados.nome;
      
 
 
